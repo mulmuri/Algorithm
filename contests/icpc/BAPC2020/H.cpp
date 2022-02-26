e82e5e0c78afdcbf6d007781271171de56fc8373
mulmuri <68294539+mulmuri@users.noreply.github.com>
2022-02-26 14:57:36 +0900

    Commits at 2022-02-26 14:57:36

---
+++ contests/icpc/BAPC2020/H.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+
+int main() {
+    int n,d;
+
+    cin >> n;
+    cin >> d; cout << d << endl;
+    for (int i=0; i<d; i++) {
+        string s; cin >> s; cout << s << endl;
+    }
+}
