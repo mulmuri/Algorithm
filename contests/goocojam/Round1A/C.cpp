668d4bcf20d8bde394a9ddb3e1e2d286b517768f
mulmuri <68294539+mulmuri@users.noreply.github.com>
2022-04-09 10:02:24 +0900

    Commits at 2022-04-09 10:02:24

---
+++ contests/goocojam/Round1A/C.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+
+void solve(int tc) {
+    cout << "Case #"<< tc << ":\n";
+}
+
+int main() {
+    int t; cin >> t;
+    for (int i=1; i<=t; i++) solve(i);
+}
