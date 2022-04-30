e287a4d7247676f9aafd95d2a424a8f28ffabdd8
mulmuri <68294539+mulmuri@users.noreply.github.com>
2022-04-30 18:23:18 +0900

    Commits at 2022-04-30 18:23:18

---
+++ contests/goocojam/Round1C/C.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <string>
+using namespace std;
+
+#define fastio                        \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);
+#define endl '\n'
+#define int long long
+
+
+
+void solve(int tc) {
+    int ans = 0;
+
+    cout << "Case #"<< tc << ": " << ans << endl;
+}
+
+int32_t main() {
+    int t; cin >> t;
+    for (int i=1; i<=t; i++) solve(i);
+}
