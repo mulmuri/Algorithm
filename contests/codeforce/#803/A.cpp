597fdb667c05bf391586f3850baf84a2b92cafcb
mulmuri <68294539+mulmuri@users.noreply.github.com>
2022-06-28 23:39:06 +0900

    Commits at 2022-06-28 23:39:06

---
+++ contests/codeforce/#803/A.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+#define fastio                        \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);
+#define endl '\n'
+#define int long long
+
+typedef long long ll;
+typedef pair<int,int> pii;
+
+const int MOD = 998232453;
+const int INF = 1e18;
+const int SIZE = 200001;
+
+
+int n;
+int arr[SIZE];
+
+
+void solve() {
+    cin >> n;
+    for (int i=1; i<=n; i++) cin >> arr[i];
+    cout << arr[1] << endl;
+}
+
+int32_t main() {
+    fastio;
+
+    int t; cin >> t;
+    while (t--) solve();
+}
