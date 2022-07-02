b9b52bbd477957f5c0d862d044fdc6e3ffdb606c
mulmuri <68294539+mulmuri@users.noreply.github.com>
2022-07-02 13:50:51 +0900

    Commits at 2022-07-02 13:50:51

---
+++ contests/ucpc/template.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+#define fastio                        \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL); \
+    cout.tie(NULL)
+#define endl '\n'
+#define int long long
+
+const int MOD = 998232453;
+const int INF = 1e18;
+const int SIZE = 200001;
+
+
+int n;
+int arr[SIZE];
+
+int32_t main() {
+    fastio;
+}
