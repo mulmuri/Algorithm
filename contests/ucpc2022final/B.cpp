8ddcbdf2624a1fcb0cd199635ce2e08363cdb6d8
mulmuri <68294539+mulmuri@users.noreply.github.com>
2022-07-23 12:00:47 +0900

    Commits at 2022-07-23 12:00:47

---
+++ contests/ucpc2022final/B.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <vector>
+
+int readInt(const std::string& code, int& index) {
+    
+}
+
+struct Node {
+    int operatorId;
+    std::vector<int> operands;
+
+    Node(const std::string& code, int& index) {
+        
+    }
+};
+
+int main() {
+    std::cin.tie(nullptr)->sync_with_stdio(false);
+    
+    int n;
+    std::cin >> n;
+    std::string code;
+    std::cin >> code;
+
+    
+
+    return 0;
+}
