b7ea0c109b3c5ecbd46940968d2ef11a17f28e41
mulmuri <68294539+mulmuri@users.noreply.github.com>
2022-07-27 22:28:16 +0900

    Commits at 2022-07-27 22:28:16

---
+++ topic/small_to_large/template.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define fastio                        \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);
+#define endl '\n'
+
+
+const int SIZE = 100001;
+
+
+
+int main() {
+
+}
