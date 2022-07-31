fe556b1d42bf985815ef88e2f8e2b930ffe2371c
mulmuri <68294539+mulmuri@users.noreply.github.com>
2022-07-31 21:01:45 +0900

    Commits at 2022-07-31 21:01:45

---
+++ contests/atcoder/ABC262/A.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+#define fastio                        \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL); \
+    cout.tie(NULL)
+#define endl '\n'
+#define int long long
+
+const int MOD = 998232453;
+const int INF = 1e18;
+const int SIZE = 300001;
+
+
+int n;
+int arr[SIZE];
+
+int32_t main() {
+    fastio;
+    
+    cin >> n;
+    if (n%4 == 2) cout << n;
+    else cout << (n + 4 - (n+2)%4);
+}
