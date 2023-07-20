07ce56ad573eddf38d7b43db7938929f9ca77bc9
mulmuri <68294539+mulmuri@users.noreply.github.com>
2023-07-20 16:51:44 +0900

    Commits at 2023-07-20 16:51:44

---
+++ contests/codeforce/#884/A.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define fastio                        \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);
+#define endl '\n'
+#define int long long
+
+typedef long long ll;
+
+const int MOD = 998232453;
+const int INF = 1e18;
+const int SIZE = 300001;
+
+
+
+
+void solve() {
+    int a,b;
+    cin >> a >> b;
+    cout << a + b << endl;
+}
+
+int32_t main() {
+    fastio;
+
+    int t; cin >> t;
+    while (t--) solve();
+}
