6cba13f92968538c90dc35cdaf68a0a22cb911ee
mulmuri <68294539+mulmuri@users.noreply.github.com>
2023-08-19 10:42:52 +0900

    Commits at 2023-08-19 10:42:52

---
+++ contests/scpc/23pre2/template.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+#define fastio                        \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL); \
+    cout.tie(NULL)
+#define endl '\n'
+#define int long long
+
+const int MOD = 998232453;
+const int INF = 1e18;
+const int SIZE = 200001;
+
+
+int n;
+int arr[SIZE];
+
+
+
+void solve(int tc) {
+    cout << "Case #" << tc << endl;
+}
+
+int32_t main() {
+    fastio;
+
+    int t; cin >> t;
+    for (int i=1; i<=t; i++) solve(i);
+}
